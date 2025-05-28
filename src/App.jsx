f0c202ce3112e4f3de367a744df3c8f37dab68ac
Horlanrewajucode <buzareeolanrewaju01@gmail.com>
2025-05-28 16:59:27 +0100

    updated adding and deleting functionality

--- src/App.jsx
+++ src/App.jsx
@@ -14,12 +14,28 @@ function App() {
     setItems((items) => [...items, item]);
   }
 
+  function handleDeleteItems(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form onAddItems={handleAddItems } />
-      <PackingList items={items} />
-      <Stats />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItems}
+        onToggleItem={handleToggleItem}
+      />
+      <Stats items={items} />
     </div>
   );
 }
@@ -28,7 +44,7 @@ function Logo() {
   return <h1>🌴Far Away 💼</h1>;
 }
 
-function Form({onAddItems}) {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
 
@@ -36,8 +52,9 @@ function Form({onAddItems}) {
     e.preventDefault();
 
     if (!description) return;
+
     const newItem = { description, quantity, packed: false, id: Date.now() };
-    console.log(newItem)
+    // console.log(newItem);
     onAddItems(newItem);
 
     setDescription("");
@@ -48,14 +65,14 @@ function Form({onAddItems}) {
     setDescription(e.target.value);
   }
 
-  function handlQuantity(e) {
+  function handleQuantity(e) {
     setQuantity(Number(e.target.value));
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your Trip🥰</h3>
-      <select value={quantity} onChange={handlQuantity}>
+      <select value={quantity} onChange={handleQuantity}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
@@ -73,35 +90,58 @@ function Form({onAddItems}) {
   );
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItem, onToggleItem }) {
   return (
     <li>
-      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => onToggleItem(item.id)}
+      />
+      <span style={item.packed ? { color: "red" } : {}}>
         {item.quantity}
         {item.description}
       </span>
-      <button>❌</button>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
 
-function PackingList({items}) {
+function PackingList({ items, onDeleteItem, onToggleItem }) {
   return (
     <div className="list">
       <ul>
         {items.map((item) => (
-          <Item item={item} key={item.id} />
+          <Item
+            item={item}
+            key={item.id}
+            onDeleteItem={onDeleteItem}
+            onToggleItem={onToggleItem}
+          />
         ))}
       </ul>
     </div>
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list🚀</em>
+      </p>
+    );
+
+  const numItem = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItem) * 100);
+
   return (
     <footer className="stats">
       <em>
-        💼You have X items on your List, and you have already packed X (X%)
+        {percentage === 100
+          ? "You got everything! Ready to go✈️"
+          : ` 💼 You have ${numItem} items on your List, and you have already packed ${numPacked} (${percentage}%)`}
       </em>
     </footer>
   );
